0de1dab68cbbba840893ac246827277c972cbf60
marty <martins.andzans@gmail.com>
2021-06-17 00:57:06 +0300

    release plugin update

--- pet-clinic-data/src/main/java/test/springframework/sfgpetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/test/springframework/sfgpetclinic/services/map/VetServiceMap.java
@@ -1,11 +1,12 @@
 package test.springframework.sfgpetclinic.services.map;
 
 import test.springframework.sfgpetclinic.model.Vet;
-import test.springframework.sfgpetclinic.services.CrudService;
+import test.springframework.sfgpetclinic.services.VetService;
 
 import java.util.Set;
 
-public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
+
     @Override
     public Set<Vet> findAll() {
         return super.findAll();
